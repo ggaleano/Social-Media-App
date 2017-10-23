d6c4e322e0b219bb13d371a2b2b8467d90addf62
Geo <ggaleano2014@fau.edu>
2017-10-22 22:37:01 -0400

    Adding caption and number of likes to display

--- Social Media/FeedVC.swift
+++ Social Media/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
 
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +24,17 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.RES_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value ?? "The DB is empty")
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshots {
+                    print("Geo: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
     }
 
@@ -35,12 +47,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         return 1
     }
     
+    // number in the array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        print("Geo: post count is \(posts.count)")
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
+        
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
--- Social Media/PostCell.swift
+++ Social Media/PostCell.swift
@@ -14,12 +14,18 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var postingImg: UIImageView!
     @IBOutlet weak var postText: UITextView!
-    @IBOutlet weak var likes: UILabel!
+    @IBOutlet weak var likesLbl: UILabel!
 
+    var post: Post!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    
+    func configureCell(post: Post) {
+        self.post = post
+        self.postText.text = post.caption
+        self.likesLbl.text = "\(post.likes)"
+    }
 }
